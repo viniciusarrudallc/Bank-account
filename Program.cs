90c891bba8afabd3ff72527de5ba884fef36e706
viniciusarrudallc <viniciusarruda.llc@gmail.com>
2026-08-17 12:02:10 -0300

    Estrutura inicial, com deposito inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿ 
+namespace Bank;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+
+       
+
+    }
+
+
+}
---
+++ Transactions.cs
@@ -0,0 +1,20 @@
+﻿namespace Banco
+{
+    internal class Transactions
+    {
+        
+        public static void Deposit(Account account)
+        {
+            Console.WriteLine("INSERT VALUE TO DEPOSIT: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            account.Balance = value; 
+            
+
+
+        }    
+    
+    
+    
+    }
+}
