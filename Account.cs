1bb84c4ee6c985367dce2c7bed0a98649b12b110
viniciusarrudallc <viniciusarruda.llc@gmail.com>
2026-08-17 14:28:44 -0300

    Fixed some things

--- Account.cs
+++ Account.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Banco
+﻿namespace Bank
 {
     internal class Account
     {
+        public int AccountId {  get; set; }
+        public string AccountHolder { get; set; }
+        public double Balance {  get; set; }
+
+
+        public Account() { }
+        
+        public Account(int AccountId, string AccountHolder, double initialDeposit)
+        {
+            this.AccountId = AccountId;
+            this.AccountHolder = AccountHolder;
+            Balance = initialDeposit;
+
+        }
+
+        public Account(int AccountId, string AccountHolder): this(AccountId,AccountHolder,0.0)
+        {
+
+        }
+
+        
+        
+        
+        
+        
+        
+        public override string ToString()
+        {
+            return "NAME: "
+                + AccountHolder
+                + ", " 
+                + "ID: " 
+                + AccountId 
+                + ", " 
+                + "BALANCE: " 
+                + Balance;
+        }
+
+        
+    
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
-﻿ 
+﻿
 namespace Bank;
 
     class Program
 {
-    static void Main(string[] args)
+   public static void Main(string[] args)
     {
+        Account account;
+        
+        Console.WriteLine("# ACCOUNT ID: ");
+        int accountId = int.Parse(Console.ReadLine());
 
+        Console.WriteLine("# ACCOUNT HOLDER");
+        string accountHolder = Console.ReadLine();
+        
+        Console.WriteLine("You need to do a security deposit? S/N ");
+        char option = char.Parse(Console.ReadLine());
        
+        if(option == 's' ||  option == 'S'){
+            Console.WriteLine("How many you want to deposit? ");
+            double initialValue = double.Parse(Console.ReadLine());
+
+           account = new Account(accountId, accountHolder, initialValue);
+        }
+        else
+        {
+            account = new Account(accountId, accountHolder);
+        }
+
+        Console.WriteLine("Deposit: ");
+        Transactions.Deposit(account);
+
+        Console.WriteLine(account);
+
+        Transactions.Withdraw(account);
+
+        Console.WriteLine(account);
 
     }
 
